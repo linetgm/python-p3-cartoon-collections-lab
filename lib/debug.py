7b471ef3957620cbbbc7ca36d8ed35a8297eba54
Linet <muthiigathoni@gmail.com>
2023-09-13 11:07:22 +0300

    Complete

---
+++ lib/debug.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from cartoon_collections import (
+    roll_call_dwarves, summon_captain_planet,
+    long_planeteer_calls, find_the_cheese
+)
+
+if __name__ == '__main__':
+    import ipdb; ipdb.set_trace()
